3f01323e324624b5ae33de410220a2b38c475b06
BackstagecrewIS <locksmith@backstagecrew.com>
2023-02-12 22:51:29 +0000

    Update for errors

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
