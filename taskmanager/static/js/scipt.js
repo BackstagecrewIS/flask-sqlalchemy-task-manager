df54d951ef95b9e87ddf45bac461555f5200e920
BackstagecrewIS <locksmith@backstagecrew.com>
2023-02-16 22:17:59 +0000

    Add Add Task functions

--- taskmanager/static/js/scipt.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // sidenav initialization
-    let sidenav = document.querySelectorAll(".sidenav");
-    M.Sidenav.init(sidenav);
-});
